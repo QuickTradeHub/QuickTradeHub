f1e19939236265e01612c81cd25ce09037656ca5
Pravin Torave <pravintorave201@gmail.com>
2025-02-11 14:37:19 +0530

    api response changed

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -2,8 +2,8 @@ export const fetchOrders = async (userId, page, pageSize) => {
     const response = await fetch(`http://13.49.132.61:5142/api/orders/user/${userId}?page=${page}&pageSize=${pageSize}`);
     const data = await response.json();
     return {
-      data: data.orders,  // Modify based on your actual API response structure
-      totalPages: data.totalPages,  // Adjust accordingly
+      data: data.data,  // Modify based on your actual API response structure
+      totalPages: data.totalCount,  // Adjust accordingly
     };
   };
   
