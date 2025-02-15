ff993cef57b4eaf9b119c7e51541882773af4934
Pravin Torave <pravintorave201@gmail.com>
2025-02-15 21:51:09 +0530

    category model updated and icon section added in it

--- ProductService/models/category.js
+++ ProductService/models/category.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
-    description: { type: String, required: true }
+    description: { type: String, required: true },
+    icon:{ type: String, required: true, unique: true }
 });
 
 module.exports = mongoose.model('Category', categorySchema);
